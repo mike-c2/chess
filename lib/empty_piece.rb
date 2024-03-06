357564da279e0948c8826ceed1c8ce5d74fda563
Mike Costa <mcosta1@gmail.com>
2024-03-05 23:33:29 -0500

    Add EmptyPiece class
    This will mostly be used as a basis for child classes. This class will
    just represent an empty square on the Chess board.

---
+++ lib/empty_piece.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+##
+# This represents an empty Chess piece.
+# This is mostly an abstract class, the real pieces will
+# inherit from this. An object of this class will be used
+# to represent an empty space on the board.
+class EmptyPiece
+  attr_accessor :current_position
+  attr_reader :board, :move_counter
+
+  @@default_board = nil
+
+  EMPTY = new
+  WHITE = 'white'
+  BLACK = 'black'
+  WHITE_FONT_COLOR = "\e[37m"
+  BLACK_FONT_COLOR = "\e[30m"
+
+  def initialize(board = @@default_board)
+    self.board = board
+    self.current_position = nil
+    self.move_counter = 0
+  end
+
+  def increment_counter; end
+
+  # These methods will be more defined in the subclasses
+  def generate_possible_moves
+    []
+  end
+
+  def check?
+    false
+  end
+
+  def same_color?(_other)
+    false
+  end
+
+  def opponents_color?(_other)
+    false
+  end
+
+  # How the piece actually looks on the board
+  def piece_str
+    ''
+  end
+end
