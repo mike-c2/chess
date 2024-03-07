11d4c6537356b2134b29cbeec63138ac27407ffd
Mike Costa <mcosta1@gmail.com>
2024-03-06 19:31:32 -0500

    Add Black module with unit tests
    All black chess pieces will implement this.

---
+++ lib/black.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require_relative 'color'
+
+##
+# This will add the color Black to a chess piece.
+module Black
+  include Color
+
+  def color
+    BLACK
+  end
+
+  def font_color
+    BLACK_FONT_COLOR
+  end
+
+  def same_side?(other)
+    color == other.color
+  end
+
+  def opponents_side?(other)
+    other.color == WHITE
+  end
+end
---
+++ spec/unit/black_spec.rb
@@ -0,0 +1,80 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/black'
+
+describe Black do
+  subject(:black_object) { black_class.new }
+  let(:black_class) { Class.new { include Black } }
+
+  describe '#color' do
+    it "only returns 'black'" do
+      result = black_object.color
+      expect(result).to eq('black')
+    end
+  end
+
+  describe '#font_color' do
+    it 'only returns \'\e[30m\'' do
+      result = black_object.font_color
+      expect(result).to eq("\e[30m")
+    end
+  end
+
+  describe '#same_side?' do
+    let(:other) { double }
+
+    context 'when the other object is not black' do
+      before do
+        allow(other).to receive(:color).and_return('white')
+      end
+
+      it 'returns false' do
+        is_expected.not_to be_same_side(other)
+      end
+    end
+
+    context 'when the other object is black' do
+      before do
+        allow(other).to receive(:color).and_return('black')
+      end
+
+      it 'returns true' do
+        is_expected.to be_same_side(other)
+      end
+    end
+  end
+
+  describe '#opponents_side?' do
+    let(:other) { double }
+
+    context 'when the other object is neither black nor white' do
+      before do
+        allow(other).to receive(:color).and_return('none')
+      end
+
+      it 'returns false' do
+        is_expected.not_to be_opponents_side(other)
+      end
+    end
+
+    context 'when the other object is black' do
+      before do
+        allow(other).to receive(:color).and_return('black')
+      end
+
+      it 'returns false' do
+        is_expected.not_to be_opponents_side(other)
+      end
+    end
+
+    context 'when the other object is white' do
+      before do
+        allow(other).to receive(:color).and_return('white')
+      end
+
+      it 'returns true' do
+        is_expected.to be_opponents_side(other)
+      end
+    end
+  end
+end
