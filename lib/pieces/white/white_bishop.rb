9a98e244368d70be614ab22b67969b73446a062b
Mike Costa <mcosta1@gmail.com>
2024-03-07 16:43:34 -0500

    Add WhiteBishop class

---
+++ lib/pieces/white/white_bishop.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative '../bishop'
+require_relative '../colors/white'
+
+##
+# Represents the White Bishop in Chess.
+class WhiteBishop < Bishop
+  include White
+end
