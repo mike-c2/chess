e8b4bd8c328d3a2995e0c17a7e237087d9bf9f80
Mike Costa <mcosta1@gmail.com>
2024-03-06 22:49:45 -0500

    Add the King class

---
+++ lib/pieces/king.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative 'empty_piece'
+
+##
+# Represents the King in Chess.
+# This is an abstract class, with
+# no color.
+class King < EmptyPiece
+  def initialize(board = self.class.default_board)
+    super
+    @piece_type = 'king'
+    @offsets = KING_OFFSETS
+  end
+end
