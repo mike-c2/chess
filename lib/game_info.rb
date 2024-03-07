6894510d4c63a798f3104e68119feff898db80a2
Mike Costa <mcosta1@gmail.com>
2024-03-06 20:00:40 -0500

    Add GameInfo module with display method
    Prints game instructions to the terminal.

---
+++ lib/game_info.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+##
+# Provides information and instruction on how
+# to play this Chess game.
+#
+# This does not explain how to actually play
+# the game of Chess, just how to make moves
+# using this version of Chess.
+module GameInfo
+  def self.display
+    puts <<~INFO
+
+      Welcome to this game of Chess!
+
+      This is can only be played in 2-player mode, there is no AI.
+
+      Enter the move you want to make using the following Chess notation.
+      For example, if you want to move the pawn on E2 up to E4, enter the
+      following notation (input is case insensitive):
+
+      E2 E4
+
+      This is how all pieces on the board are moved, by entering the
+      starting location followed by the ending location.  This includes
+      moves that captures an opponent's piece and castling.
+
+      For castling, move the King over to the left or right 2 spaces,
+      then the Rook will automatically move as well.
+
+      For pawn promotions, you will be prompted to select what the new
+      piece will be.
+
+    INFO
+  end
+end
