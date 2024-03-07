48eecb08f8afc206896b07ee34e9655cfa1a91a7
Mike Costa <mcosta1@gmail.com>
2024-03-07 16:52:31 -0500

    Add BlackRook class

---
+++ lib/pieces/black/black_rook.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative '../rook'
+require_relative '../colors/black'
+
+##
+# Represents the Black Rook in Chess.
+class BlackRook < Rook
+  include Black
+end
