91b5c639f8687e8f1e46eb89425769412c54fee2
Mike Costa <mcosta1@gmail.com>
2024-03-07 16:52:17 -0500

    Add BlackKnight class

---
+++ lib/pieces/black/black_knight.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative '../knight'
+require_relative '../colors/black'
+
+##
+# Represents the Black Knight in Chess.
+class BlackKnight < Knight
+  include Black
+end
