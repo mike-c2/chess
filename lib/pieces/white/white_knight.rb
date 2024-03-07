1300655bfbb92d6a929ffdfdaaa9d7b379591d7a
Mike Costa <mcosta1@gmail.com>
2024-03-07 16:43:51 -0500

    Add WhiteKnight class

---
+++ lib/pieces/white/white_knight.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative '../knight'
+require_relative '../colors/white'
+
+##
+# Represents the White Knight in Chess.
+class WhiteKnight < Knight
+  include White
+end
