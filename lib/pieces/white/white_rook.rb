e0f2a67a0d036bdf7658d50e07c89b9d6665d775
Mike Costa <mcosta1@gmail.com>
2024-03-07 16:44:12 -0500

    Add WhiteRook class

---
+++ lib/pieces/white/white_rook.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative '../rook'
+require_relative '../colors/white'
+
+##
+# Represents the White Rook in Chess.
+class WhiteRook < Rook
+  include White
+end
