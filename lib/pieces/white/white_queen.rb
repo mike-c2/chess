752d17ef22e0f734e64a6d144e116151801104c6
Mike Costa <mcosta1@gmail.com>
2024-03-07 16:43:15 -0500

    Add WhiteQueen class

---
+++ lib/pieces/white/white_queen.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative '../queen'
+require_relative '../colors/white'
+
+##
+# Represents the White Queen in Chess.
+class WhiteQueen < Queen
+  include White
+end
