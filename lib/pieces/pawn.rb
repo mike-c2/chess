dc1668ef18bc315ea875fa899b28977fa9072b49
Mike Costa <mcosta1@gmail.com>
2024-03-07 16:35:36 -0500

    Add Pawn class

---
+++ lib/pieces/pawn.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative 'empty_piece'
+
+##
+# Represents the Pawn in Chess.
+# This is an abstract class, with
+# no color.
+class Pawn < EmptyPiece
+  def initialize(board = self.class.default_board)
+    super
+    self.piece_type = 'pawn'
+  end
+end
