c734f5c2ba1ee94d42d2ff1748018bcdc74f31c6
Mike Costa <mcosta1@gmail.com>
2024-03-07 16:34:24 -0500

    Add Queen class

---
+++ lib/pieces/queen.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative 'empty_piece'
+
+##
+# Represents the Queen in Chess.
+# This is an abstract class, with
+# no color.
+class Queen < EmptyPiece
+  def initialize(board = self.class.default_board)
+    super
+    self.piece_type = 'queen'
+    @offsets = QUEEN_OFFSETS
+  end
+end
