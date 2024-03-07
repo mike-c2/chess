bc32ef741a40d34898d3cb4c2bd4069db30022cf
Mike Costa <mcosta1@gmail.com>
2024-03-07 17:04:37 -0500

    Add all_colored_pieces.rb file
    This will reduce the number of require_relative lines in other files.

---
+++ lib/pieces/all_colored_pieces.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# This just makes it easier to include all
+# of the different Chess Piece classes.
+
+require_relative 'white/white_king'
+require_relative 'white/white_queen'
+require_relative 'white/white_bishop'
+require_relative 'white/white_knight'
+require_relative 'white/white_rook'
+require_relative 'white/white_pawn'
+
+require_relative 'black/black_king'
+require_relative 'black/black_queen'
+require_relative 'black/black_bishop'
+require_relative 'black/black_knight'
+require_relative 'black/black_rook'
+require_relative 'black/black_pawn'
