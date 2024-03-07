319e7ffd7ddd64637c36ee6d969cf98637c0543a
Mike Costa <mcosta1@gmail.com>
2024-03-07 16:34:42 -0500

    Add Bishop class

---
+++ lib/pieces/bishop.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative 'empty_piece'
+
+##
+# Represents the Bishop in Chess.
+# This is an abstract class, with
+# no color.
+class Bishop < EmptyPiece
+  def initialize(board = self.class.default_board)
+    super
+    self.piece_type = 'bishop'
+    @offsets = BISHOP_OFFSETS
+  end
+end
