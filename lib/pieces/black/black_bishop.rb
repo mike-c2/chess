fff2a54031b5fbc1618cc80a221e29d7cd49f440
Mike Costa <mcosta1@gmail.com>
2024-03-07 16:51:51 -0500

    Add BlackBishop class

---
+++ lib/pieces/black/black_bishop.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative '../bishop'
+require_relative '../colors/black'
+
+##
+# Represents the Black Bishop in Chess.
+class BlackBishop < Bishop
+  include Black
+end
