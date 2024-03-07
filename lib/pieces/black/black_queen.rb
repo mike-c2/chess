880e66c39c4d80eef5b8fdf174afc65c418a9381
Mike Costa <mcosta1@gmail.com>
2024-03-07 16:51:31 -0500

    Add BlackQueen class

---
+++ lib/pieces/black/black_queen.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative '../queen'
+require_relative '../colors/black'
+
+##
+# Represents the Black Queen in Chess.
+class BlackQueen < Queen
+  include Black
+end
