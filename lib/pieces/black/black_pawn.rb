c99d564f1456a0e35f00a5fb623fbaf509028796
Mike Costa <mcosta1@gmail.com>
2024-03-07 16:52:52 -0500

    Add BlackPawn class

---
+++ lib/pieces/black/black_pawn.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative '../pawn'
+require_relative '../colors/black'
+
+##
+# Represents the Black Pawn in Chess.
+class BlackPawn < Pawn
+  include Black
+end
