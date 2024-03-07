b24682ed78bd99b04abfb6626e1b2da4f5973097
Mike Costa <mcosta1@gmail.com>
2024-03-07 16:44:23 -0500

    Add WhitePawn class

---
+++ lib/pieces/white/white_pawn.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative '../pawn'
+require_relative '../colors/white'
+
+##
+# Represents the White Pawn in Chess.
+class WhitePawn < Pawn
+  include White
+end
