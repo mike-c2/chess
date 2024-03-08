1fea127332e6767838bcf54235e93706711c2fd5
Mike Costa <mcosta1@gmail.com>
2024-03-07 22:32:28 -0500

    Add piece font_color to the row being printed
    The font_color was removed from EmptyPiece#piece_str because it was
    messing up  the String#center call within the same method.  The center
    takes into account all characters in the string, including formatting
    characters that do not get directly displayed to the terminal.  So now
    the font_color cannot be part of the string being centered and must be
    applied separately on the string concatenation line.

--- lib/board_display.rb
+++ lib/board_display.rb
@@ -62,7 +62,7 @@ class BoardDisplay
     chess_row.each_with_index do |piece, index|
       current_color = index.even? ? bg_colors.first : bg_colors.last
       piece_formatted = piece.piece_str.center(7)
-      row_string = "#{row_string}#{current_color}#{piece_formatted}#{NO_COLOR}"
+      row_string = "#{row_string}#{current_color}#{piece.font_color}#{piece_formatted}#{NO_COLOR}"
     end
 
     "#{row_string} #{row_label}\n   #{padding}"
