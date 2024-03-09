e70f3ff424c9160e94e64500862e3178bcf16e1f
Mike Costa <mcosta1@gmail.com>
2024-03-09 10:23:43 -0500

    Add SingleMovePiece class
    King and Knight will now inherit from this class.  Each piece will
    calculate all of its possible moves.  Since the algorithm for doing
    this would be the same for King and Knight, this parent class was
    created to handle that.
    
    Note:  King and Knight each have their own offsets, so they do not move
    the same on the board.  The algorithm would calculate the possible moves
    by looking at these offsets; the algorithm would be the same.

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-require_relative 'empty_piece'
+require_relative 'single_move_piece'
 
 ##
 # Represents the Knight in Chess.
 # This is an abstract class, with
 # no color.
-class Knight < EmptyPiece
+class Knight < SingleMovePiece
   def initialize(position = nil, board = self.class.default_board)
     super
     self.piece_type = 'knight'
---
+++ lib/pieces/single_move_piece.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative 'empty_piece'
+
+##
+# This represents a piece in Chess that
+# only moves one space at a time.
+#
+# For example, King and Knight.
+class SingleMovePiece < EmptyPiece
+end
