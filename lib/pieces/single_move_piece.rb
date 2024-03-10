48d07a358c9003a2962e7efd365871c0c5528dad
Mike Costa <mcosta1@gmail.com>
2024-03-10 10:38:56 -0400

    Add SingleMovePiece#generate_possible_moves method
    The King and Knight pieces now know how to move.

--- lib/pieces/single_move_piece.rb
+++ lib/pieces/single_move_piece.rb
@@ -8,4 +8,14 @@ require_relative 'empty_piece'
 #
 # For example, King and Knight.
 class SingleMovePiece < EmptyPiece
+  def generate_possible_moves
+    possible_moves = []
+
+    @offsets.each do |offset|
+      next_move = generate_potential_move(offset)
+      possible_moves << next_move if next_move
+    end
+
+    possible_moves
+  end
 end
