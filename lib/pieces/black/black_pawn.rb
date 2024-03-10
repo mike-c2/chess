2147bda32ed1b14d44ef3624eba24e41bcec640c
Mike Costa <mcosta1@gmail.com>
2024-03-10 18:40:51 -0400

    Add BlackPawn specific properties
    Pawns move in unique ways, so they require specialized properties.

--- lib/pieces/black/black_pawn.rb
+++ lib/pieces/black/black_pawn.rb
@@ -7,4 +7,13 @@ require_relative '../../colors/black'
 # Represents the Black Pawn in Chess.
 class BlackPawn < Pawn
   include Black
+
+  def initialize(position = nil, board = self.class.default_board)
+    super
+    @single_move_offset = [-1, 0]
+    @double_move_offset = [-2, 0]
+    @kill_offsets = [[-1, -1], [-1, 1]]
+    @double_move_row = 6
+    @promotion_row = 0
+  end
 end
