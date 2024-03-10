7ca7010967fd3c4e035e0cfe303eacce4a91750c
Mike Costa <mcosta1@gmail.com>
2024-03-10 16:56:47 -0400

    Add MultipleMovePiece#generate_possible_moves
    Queens, Rooks, and Bishops can now move.

--- lib/pieces/multiple_move_piece.rb
+++ lib/pieces/multiple_move_piece.rb
@@ -8,4 +8,36 @@ require_relative 'empty_piece'
 #
 # For example: Queen, Bishop, and Rook.
 class MultipleMovePiece < EmptyPiece
+  def generate_possible_moves
+    possible_moves = []
+
+    @offsets.each do |offset|
+      multiple_moves_at_a_time = generate_possible_multiple_moves(offset)
+      possible_moves.concat(multiple_moves_at_a_time)
+    end
+
+    possible_moves
+  end
+
+  private
+
+  def generate_possible_multiple_moves(offset)
+    possible_moves = []
+
+    total_offset = [0, 0]
+
+    loop do
+      total_offset = [total_offset.first + offset.first, total_offset.last + offset.last]
+
+      next_move = generate_potential_move(total_offset)
+      break unless next_move
+
+      possible_moves << next_move
+
+      # cannot jump over enemy pieces
+      break if @board.get(next_move.ending_position)
+    end
+
+    possible_moves
+  end
 end
