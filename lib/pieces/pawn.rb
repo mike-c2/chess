1df8f691cff32447c7429008b17ff053b28f253e
Mike Costa <mcosta1@gmail.com>
2024-03-10 20:56:41 -0400

    Add promotion methods to Pawn class
    These are needed for when the pawn reaches the end of the board.

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -2,6 +2,7 @@
 
 require_relative 'empty_piece'
 require_relative '../positionals/move'
+require_relative '../board/piece_maker'
 
 ##
 # Represents the Pawn in Chess.
@@ -16,10 +17,24 @@ class Pawn < EmptyPiece
     @kill_offsets = []
     @double_move_row = -1
     @promotion_row = -1
+    @piece_maker = PieceMaker.new(@board)
   end
 
   def generate_possible_moves
     no_kill_moves + kill_moves
+  end
+
+  def promote
+    return unless position.array_notation.first == @promotion_row
+
+    choice = promotion_prompt
+    process_promotion(choice)
+  end
+
+  def no_prompt_promote
+    return unless position.array_notation.first == @promotion_row
+
+    process_promotion('queen')
   end
 
   private
@@ -64,5 +79,40 @@ class Pawn < EmptyPiece
     return if @board.get(next_position)
 
     Move.create(position, next_position)
+  end
+
+  def process_promotion(type)
+    @piece_maker.created?(color: color, type: type, location: position.chess_notation)
+    new_piece = @board.get(position)
+
+    puts "\n#{self} has been promoted to #{new_piece}"
+  end
+
+  def promotion_prompt
+    promotion_instructions
+    options = { 'q' => 'queen', 'r' => 'rook', 'b' => 'bishop', 'k' => 'knight' }
+
+    loop do
+      choice = gets.chomp.strip.downcase
+      choice = choice[0]
+      return options[choice] if %w[q r b k].include?(choice)
+
+      puts 'What you entered is not valid, try again.'
+    end
+  end
+
+  def promotion_instructions
+    puts <<~INSTRUCTIONS
+      The #{self} will now be promoted, enter the piece that you want.
+
+      Enter 'q' for queen
+      Enter 'r' for rook
+      Enter 'b' for bishop
+      Enter 'k' for knight
+
+      Anything after the first letter will be ignored, so 'qabc' will
+      mean queen.  The input is not case sensitive.
+
+    INSTRUCTIONS
   end
 end
