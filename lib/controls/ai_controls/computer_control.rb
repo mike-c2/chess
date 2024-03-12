75de6ca32138b9ac244d1a84a9954d5edd0fddb1
Mike Costa <mcosta1@gmail.com>
2024-03-11 22:44:39 -0400

    Add ComputerControl class
    This will provide AI control for playing chess against the computer. It
    just makes random moves, so is not a very good opponent.

---
+++ lib/controls/ai_controls/computer_control.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require_relative '../control'
+
+##
+# Manages the computer's turn
+# The computer will just randomly pick a valid move
+# This is an abstract class
+class ComputerControl < Control
+  def play?
+    possible_moves = valid_moves
+    return false if possible_moves.empty?
+
+    possible_moves.sample.move
+
+    promote_eligible_pieces
+
+    true
+  end
+end
