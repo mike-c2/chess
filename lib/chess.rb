1a641d96650a654eb2e7e26613810fe48bea6f0c
Mike Costa <mcosta1@gmail.com>
2024-03-12 16:13:38 -0400

    Add new game modes to the Chess class
    The game can now be played either in 2 player mode or against the
    computer.  The computer can be either white or black.

--- lib/chess.rb
+++ lib/chess.rb
@@ -2,18 +2,24 @@
 
 require_relative 'controls/black_control'
 require_relative 'controls/white_control'
+require_relative 'controls/ai_controls/computer_black_control'
+require_relative 'controls/ai_controls/computer_white_control'
 require_relative 'game_info'
 
 ##
 # Runs the chess game
 class Chess
-  attr_accessor :white_control, :black_control
-  attr_reader :board, :active_control
+  attr_reader :board, :white_control, :black_control, :active_control
 
   def initialize(board)
     @board = board
-    @white_control = WhiteControl.new(@board)
-    @black_control = BlackControl.new(@board)
+    @human_white = WhiteControl.new(@board)
+    @human_black = BlackControl.new(@board)
+    @computer_white = ComputerWhiteControl.new(@board)
+    @computer_black = ComputerBlackControl.new(@board)
+
+    @white_control = @human_white
+    @black_control = @human_black
 
     @active_control = @white_control
   end
@@ -29,10 +35,10 @@ class Chess
   end
 
   def end_game
-    no_white_moves = white_control.valid_moves.empty?
-    no_black_moves = black_control.valid_moves.empty?
-    white_in_check = white_control.in_check?
-    black_in_check = black_control.in_check?
+    no_white_moves = @white_control.valid_moves.empty?
+    no_black_moves = @black_control.valid_moves.empty?
+    white_in_check = @white_control.in_check?
+    black_in_check = @black_control.in_check?
 
     if no_white_moves && white_in_check then puts 'Checkmate! Black won the game'
     elsif no_black_moves && black_in_check then puts 'Checkmate! White won the game'
@@ -42,8 +48,8 @@ class Chess
 
   def move_made?
     board.display
-    move_made = active_control.play?
-    puts 'Check!' if active_control.opponent_in_check?
+    move_made = @active_control.play?
+    puts 'Check!' if @active_control.opponent_in_check?
 
     return false unless move_made
 
@@ -53,11 +59,11 @@ class Chess
   end
 
   def blacks_turn
-    @active_control = black_control
+    @active_control = @black_control
   end
 
   def whites_turn
-    @active_control = white_control
+    @active_control = @white_control
   end
 
   def next_turn
@@ -74,5 +80,28 @@ class Chess
 
     whites_turn if color == 'white'
     blacks_turn if color == 'black'
+  end
+
+  def human_vs_human
+    game_mode(@human_white, @human_black)
+  end
+
+  def human_vs_computer_white
+    game_mode(@computer_white, @human_black)
+  end
+
+  def human_vs_computer_black
+    game_mode(@human_white, @computer_black)
+  end
+
+  private
+
+  def game_mode(white_control, black_control)
+    @white_control = white_control
+    @black_control = black_control
+
+    # Double next_turn will fix a stale active_control
+    next_turn
+    next_turn
   end
 end
