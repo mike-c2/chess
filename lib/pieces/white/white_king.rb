41bbc94d38e1619b925b83051e2dfa63f07c8591
Mike Costa <mcosta1@gmail.com>
2024-03-14 20:55:24 -0400

    Add castling objects to the WhiteKing class

--- lib/pieces/white/white_king.rb
+++ lib/pieces/white/white_king.rb
@@ -2,9 +2,19 @@
 
 require_relative '../king'
 require_relative '../../colors/white'
+require_relative '../../positionals/position'
 
 ##
 # Represents the White King in Chess.
 class WhiteKing < King
   include White
+
+  def initialize(position = nil, board = self.class.default_board, move_count = 0)
+    super
+
+    castling_position = Position.create_with_chess_notation('E1')
+
+    @kingside_castle = KingsideCastle.new(self, castling_position, @board)
+    @queenside_castle = QueensideCastle.new(self, castling_position, @board)
+  end
 end
