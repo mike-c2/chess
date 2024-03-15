995756666ee2c26cf8e727744abadc4c9f7aa268
Mike Costa <mcosta1@gmail.com>
2024-03-14 20:52:39 -0400

    Add King#generate_castling_moves method
    Generating castling moves cannot be included with the
    generate_possible_moves method because the check test results in an
    infinite recursive loop.  So castling moves need to be handled
    separately.

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'single_move_piece'
+require_relative '../positionals/castling_move'
 
 ##
 # Represents the King in Chess.
@@ -11,5 +12,20 @@ class King < SingleMovePiece
     super
     self.piece_type = 'king'
     @offsets = KING_OFFSETS
+
+    @kingside_castle = nil
+    @queenside_castle = nil
+  end
+
+  def generate_castling_moves
+    castling_moves = []
+
+    kingside_castle_move = @kingside_castle.generate_move
+    queenside_castle_move = @queenside_castle.generate_move
+
+    castling_moves << kingside_castle_move if kingside_castle_move
+    castling_moves << queenside_castle_move if queenside_castle_move
+
+    castling_moves
   end
 end
